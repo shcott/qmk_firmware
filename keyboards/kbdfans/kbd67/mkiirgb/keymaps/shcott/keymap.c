7e062659e4937c2c19009a2beb08a567dd20a5e9
Scott <scott.huang.21@outlook.com>
2021-03-16 18:31:40 -0500

    Change grave to delete; minor spacing

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/shcott/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/shcott/keymap.c
@@ -15,23 +15,23 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		[_LY_0] = LAYOUT_65_ansi_blocker( /* Base */
-			KC_GESC,     KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,  KC_BSLASH, KC_GRAVE,\
+			KC_GESC,     KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,  KC_BSLASH, KC_DEL,\
 		    KC_TAB,      KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  MC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC, KC_BSPC,   KC_HOME,\
 			MC_CTLX,     KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    MC_J,  MC_K,    MC_L,     KC_SCLN,  KC_QUOT,           KC_ENT,    KC_END,\
 		    KC_LSFT,     KC_Z,     KC_X,     KC_C,  KC_V,  KC_B,  KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,           KC_UP,     MO(_LY_1),\
-		    KC_LCTL,     KC_LGUI,  KC_LALT,                   KC_SPC,                      MO(_LY_2),   KC_RCTL,   KC_LEFT,     KC_DOWN,   KC_RIGHT),
+		    KC_LCTL,     KC_LGUI,  KC_LALT,                   KC_SPC,                      MO(_LY_2),   KC_RCTL,     KC_LEFT,     KC_DOWN,   KC_RIGHT),
 		[_LY_1] = LAYOUT_65_ansi_blocker( /* FN 1 */
-			RESET,       KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_TRNS,  KC_TRNS,\
-		    KC_TRNS,     RGB_VAD,  RGB_VAI, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK,  KC_PAUS, KC_TRNS,  KC_PGUP,\
+			RESET,       KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  BS_SWAP,  KC_TRNS,\
+		    KC_TRNS,     RGB_VAD,  RGB_VAI, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK,  KC_PAUS, BS_NORM,  KC_PGUP,\
 			KC_CAPS,     RGB_RMOD, RGB_MOD, RGB_SPD, RGB_SPI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          EEP_RST,  KC_PGDN,\
-		    KC_TRNS,     RGB_TOG,  RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_MUTE,          KC_VOLU,  KC_TRNS,\
+		    KC_TRNS,     RGB_TOG,  RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_MUTE,          KC_VOLU,  KC_NO,\
 		    KC_TRNS,     KC_TRNS,  KC_TRNS,                   KC_TRNS,                   KC_TRNS,          KC_TRNS,  KC_MPRV,          KC_VOLD,  KC_MNXT),
 		[_LY_2] = LAYOUT_65_ansi_blocker( /* FN 2 */
 			KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_DEL,   KC_TRNS,\
 		    KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_DEL,   KC_PGUP,\
 			KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          KC_PSCR,  KC_PGDN,\
 		    KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          KC_TRNS,  KC_TRNS,\
-		    KC_TRNS,     KC_TRNS,  KC_TRNS,                   KC_TRNS,                   KC_TRNS,          KC_TRNS,  KC_TRNS,          KC_TRNS,  KC_TRNS),
+		    KC_TRNS,     KC_TRNS,  KC_TRNS,                   KC_TRNS,                   KC_NO,            KC_TRNS,  KC_TRNS,          KC_TRNS,  KC_TRNS),
 };
 
 static uint8_t ctlx_tracker = 0;
